d3a178fe981f9dd9eea1adba886336e37b988c57
amcadmus <amcadmus@gmail.com>
2014-04-28 16:08:31 +0200

    add fils, not fully tested

---
+++ committor/template.alanine/parameters.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+fht_equi_dir=/home/wanghan/study/noneq.msm/committor/equi.system/nowater.alanine/nvt.1000ns.confs
+fht_equi_frame_traj=$fht_equi_dir/equi.frame.trr
+fht_equi_frame_count=$fht_equi_dir/equi.frame.cnt
+fht_coreset_data=$fht_equi_dir/coreset.all.dat
+fht_coreset_target=2
+fht_coreset_notin=0
+fht_num_conf_use=5000
+
+fht_integrator=sd1
+fht_dt=0.0005			# ps
+fht_stop_time=1.0		# ps
+fht_frame_feq=0.005		# ps
+fht_energy_feq=10.0		# ps
+
+fht_noSdRange=0.0		# nm
+fht_T=300			# K
+fht_taut=0.1			# ps
+fht_barostat=no			# Parrinello-Rahman or no
+fht_taup=2.0			# ps
+
+fht_meta_low=150		# deg.
+fht_meta_up=-150		# deg.
+
+# parameters for bases 
+fht_base_phi_number=8
+fht_base_phi_k_file=base.k.phi
+fht_base_psi_number=8
+fht_base_psi_k_file=base.k.psi
+
+fht_parallel_num_pro=1		# n
+fht_parallel_my_id=0		# n
+
+grompp_command="grompp"
+mdrun_command="mdrun -nt 1"
+
